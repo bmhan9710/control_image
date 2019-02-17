7df58b8698a14dded2b064c0ee477557593162be
Byungmin Han <bmhan9710@hotmail.com>
2019-02-18 01:19:25 +0900

    Added functions.

--- Control_Image/src/Get_Image.java
+++ Control_Image/src/Get_Image.java
@@ -3,6 +3,8 @@ import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.awt.image.RenderedImage;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 import javax.imageio.ImageIO;
@@ -17,13 +19,13 @@ public class Get_Image {
 		String path = "/Users/bmhan/Downloads/20190125_160513_";
 		
 		Get_Image getImg = new Get_Image();
-		getImg.getFileName(path);
+		getImg.image_resize(path);
 		
 		System.out.print("Hello World");
 		
 	}
 		
-	public void getFileName(String path) throws IOException{
+	public void image_resize(String path) throws IOException{
 		
 		File dir = new File(path);
 		File[] fileList = dir.listFiles();
@@ -49,11 +51,103 @@ public class Get_Image {
 				g.drawImage(resizeImage, 0, 0, null);
 				g.dispose();
 				//5. 새로 생성한 이미지를 파일로 저장하기
-				ImageIO.write((RenderedImage) newImage, "jpg", new File("/Users/bmhan/Downloads/20190125_160513_/New_File" + i + ".jpg"));
+				ImageIO.write((RenderedImage) newImage, "jpg", new File(path + "/New_File" + i + ".jpg"));
 				
 			}else{
 			}
 		}
 
+	}
+	
+	public void getFileName(String path){
+		
+		File dir = new File(path);
+		File []fileList = dir.listFiles();
+		
+		for(File file : fileList){		// Reads all files in directory
+			if(file.isDirectory()){
+				String inDir = path + "/" + file.getName() + "";
+				
+				System.out.println("Directory Name :" + inDir);
+				try {
+					getFileName(inDir);
+				}catch(java.lang.NullPointerException e) {
+				}
+			}else{
+				String testName = file.getName();
+				System.out.print("Parent File Name :" + file.getParent() + "--->");
+				System.out.println("File Name :" + testName);
+				
+				//if(checkFileName(testName)==true) {
+					System.out.println("Target File");
+					fileRename2(file);
+					System.out.println("Updated file Name : " + file.getName());
+				//}
+				
+				
+				//fileCopy(file.getAbsolutePath(), "/Users/IBM_ADMIN/Desktop/SoW/" + "[계약서]" + file.getName());
+				
+			}
+		}
+	}
+	
+	public boolean checkFileName(String fileName) {
+		// 2016-xx-xx -> 20160348
+		boolean isTarget=false;
+		if(fileName.charAt(4) == '-' && fileName.charAt(7) == '-') {
+			isTarget=true;
+		}
+		return isTarget;
+	}
+
+	public void fileCopy(String inFileName, String outFileName) {
+		try{
+				FileInputStream fis = new FileInputStream(inFileName);
+				FileOutputStream fos = new FileOutputStream(outFileName);
+
+				int data = 0;
+				while((data=fis.read())!=-1) {
+					fos.write(data);
+				}
+				fis.close();
+				fos.close();
+		   
+		}catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void fileRename(File mainFile) {
+		String temp = "";
+		for(int i=0; i<mainFile.getName().length(); i++) {
+			if(mainFile.getName().charAt(i)=='_') {
+				temp = temp + '_';
+				temp = temp + "1H-ECP_";
+			}else {
+				temp = temp + mainFile.getName().charAt(i);
+			}
+		}
+		File tempFile = new File(mainFile.getParent() + "/" +  temp);
+		mainFile.renameTo(tempFile);
+		
+	}
+	
+	/*public void fileRename(File mainFile) {
+		String temp = "";
+		for(int i=0; i<mainFile.getName().length(); i++) {
+			if(mainFile.getName().charAt(i)=='-') {
+			}else {
+				temp = temp + mainFile.getName().charAt(i);
+			}
+		}
+		File tempFile = new File(mainFile.getParent() + temp);
+		mainFile.renameTo(tempFile);
+		
+	}*/
+	
+	public void fileRename2(File mainFile) {
+		File tempFile = new File(mainFile.getParent() + "/20060621_" + mainFile.getName());
+		mainFile.renameTo(tempFile);
 	}
 }
