a86128cd9c3cd3c757883250e6c2f4d6a277e9d8
Byungmin Han <bmhan9710@hotmail.com>
2019-04-29 01:41:56 +0900

    Cleaned up code.

--- Control_Image/src/Get_Image.java
+++ Control_Image/src/Get_Image.java
@@ -3,236 +3,228 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.awt.image.RenderedImage;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URI;
 import java.net.URL;
+import java.net.URLConnection;
 
 import javax.imageio.ImageIO;
 
-
-
-
 public class Get_Image {
-	public static void main (String[] args) throws IOException{
-		
-		
+	public static void main(String[] args) throws IOException {
+
 		String path = "/Users/bmhan/Downloads/20190125_160513_";
-		
+
 		Get_Image getImg = new Get_Image();
-		//getImg.image_resize(path);
-		
-		
+		// getImg.image_resize(path);
+
 		System.out.print("Hello World");
-		
+
 		String uri = "http://www.ilbe.com/11138992754/widgets/keyword_rank/skins/default/img/search_icon.png";
-		
-		
+
 		getImg.openWebpage(uri);
-		
-		//getImg.fileCopy("https://toonkor.blue/data/wtoon/20180221_215724_21473.jpg", "/Users/bmhan/Downloads/test_result.jpg");
+
+		// getImg.fileCopy("https://toonkor.blue/data/wtoon/20180221_215724_21473.jpg",
+		// "/Users/bmhan/Downloads/test_result.jpg");
 		System.out.print("Complete");
 	}
-	
+
 	public static void openWebpage(String urlString) {
-	    try {
-	        Desktop.getDesktop().browse(new URL(urlString).toURI());
-	    } catch (Exception e) {
-	        e.printStackTrace();
-	    }
+		try {
+			Desktop.getDesktop().browse(new URL(urlString).toURI());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
-	
-	public void image_resize(String path) throws IOException{
-		
+
+	public void image_resize(String path) throws IOException {
+
 		File dir = new File(path);
 		File[] fileList = dir.listFiles();
 
-		
-		for(int i=0; i<fileList.length; i++){
+		for (int i = 0; i < fileList.length; i++) {
 			File file = fileList[i];
-			if(file.isFile()){
+			if (file.isFile()) {
 				System.out.println("File Name :" + path + file.getName());
 				String eachFileName = file.getAbsoluteFile().toString();
 				System.out.println(eachFileName);
-				
-				
-				//1. 파일에서 이미지 불러오기
+
+				// 1. 파일에서 이미지 불러오기
 				Image orginalImage = ImageIO.read(new File(eachFileName));
-				//2. 이미지 사이즈 수정
-				Image resizeImage = orginalImage.getScaledInstance((int) (orginalImage.getWidth(null)*(double)1.4), orginalImage.getHeight(null), Image.SCALE_SMOOTH);	
-				//속도보다 이미지 부드러움 우선 (SCALE_AREA_AVERAGING, SCALE_DEFAULT, SCALE_FAST, SCALE_REPLICATE, SCALE_SMOOTH 중에 선택)
-				//3. 결과물을 옮길 이미지 생성
-				BufferedImage newImage = new BufferedImage( (int) (orginalImage.getWidth(null)*(double)1.4), orginalImage.getHeight(null), BufferedImage.TYPE_INT_RGB );
-				//4. 생성한 이미지에 크기 수정된 이미지 그리기
+				// 2. 이미지 사이즈 수정
+				Image resizeImage = orginalImage.getScaledInstance((int) (orginalImage.getWidth(null) * (double) 1.4),
+						orginalImage.getHeight(null), Image.SCALE_SMOOTH);
+				// 속도보다 이미지 부드러움 우선 (SCALE_AREA_AVERAGING, SCALE_DEFAULT, SCALE_FAST,
+				// SCALE_REPLICATE, SCALE_SMOOTH 중에 선택)
+				// 3. 결과물을 옮길 이미지 생성
+				BufferedImage newImage = new BufferedImage((int) (orginalImage.getWidth(null) * (double) 1.4),
+						orginalImage.getHeight(null), BufferedImage.TYPE_INT_RGB);
+				// 4. 생성한 이미지에 크기 수정된 이미지 그리기
 				Graphics g = newImage.getGraphics();
 				g.drawImage(resizeImage, 0, 0, null);
 				g.dispose();
-				//5. 새로 생성한 이미지를 파일로 저장하기
+				// 5. 새로 생성한 이미지를 파일로 저장하기
 				ImageIO.write((RenderedImage) newImage, "jpg", new File(path + "/New_File" + i + ".jpg"));
-				
-			}else{
+
+			} else {
 			}
 		}
 
 	}
-	
-	public void getFileName(String path){
-		
+
+	public void getFileName(String path) {
+
 		File dir = new File(path);
-		File []fileList = dir.listFiles();
-		
-		for(File file : fileList){		// Reads all files in directory
-			if(file.isDirectory()){
+		File[] fileList = dir.listFiles();
+
+		for (File file : fileList) { // Reads all files in directory
+			if (file.isDirectory()) {
 				String inDir = path + "/" + file.getName() + "";
-				
+
 				System.out.println("Directory Name :" + inDir);
 				try {
 					getFileName(inDir);
-				}catch(java.lang.NullPointerException e) {
+				} catch (java.lang.NullPointerException e) {
 				}
-			}else{
+			} else {
 				String testName = file.getName();
 				System.out.print("Parent File Name :" + file.getParent() + "--->");
 				System.out.println("File Name :" + testName);
-				
-				//if(checkFileName(testName)==true) {
-					System.out.println("Target File");
-					fileRename2(file);
-					System.out.println("Updated file Name : " + file.getName());
-				//}
-				
-				
-				//fileCopy(file.getAbsolutePath(), "/Users/IBM_ADMIN/Desktop/SoW/" + "[계약서]" + file.getName());
-				
+
+				// if(checkFileName(testName)==true) {
+				System.out.println("Target File");
+				fileRename2(file);
+				System.out.println("Updated file Name : " + file.getName());
+				// }
+
+				// fileCopy(file.getAbsolutePath(), "/Users/IBM_ADMIN/Desktop/SoW/" + "[계약서]" +
+				// file.getName());
+
 			}
 		}
 	}
-	
+
 	public boolean checkFileName(String fileName) {
 		// 2016-xx-xx -> 20160348
-		boolean isTarget=false;
-		if(fileName.charAt(4) == '-' && fileName.charAt(7) == '-') {
-			isTarget=true;
+		boolean isTarget = false;
+		if (fileName.charAt(4) == '-' && fileName.charAt(7) == '-') {
+			isTarget = true;
 		}
 		return isTarget;
 	}
 
-	public void fileCopy(String inFileName, String outFileName){
-		try{
-				FileInputStream fis = new FileInputStream(inFileName);
-				FileOutputStream fos = new FileOutputStream(outFileName);
+	public void fileCopy(String inFileName, String outFileName) {
+		try {
+			FileInputStream fis = new FileInputStream(inFileName);
+			FileOutputStream fos = new FileOutputStream(outFileName);
 
-				int data = 0;
-				while((data=fis.read())!=-1) {
-					fos.write(data);
-				}
-				fis.close();
-				fos.close();
-		   
-		}catch (IOException e) {
+			int data = 0;
+			while ((data = fis.read()) != -1) {
+				fos.write(data);
+			}
+			fis.close();
+			fos.close();
+
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
-	public void fileRename(File mainFile) {	// REQUIRE MODIFICATION OF PARAMETER (File mainFile, String Concat_Str)
+
+	public void fileRename(File mainFile) { // REQUIRE MODIFICATION OF PARAMETER (File mainFile, String Concat_Str)
 		String temp = "";
-		for(int i=0; i<mainFile.getName().length(); i++) {
-			if(mainFile.getName().charAt(i)=='_') {
+		for (int i = 0; i < mainFile.getName().length(); i++) {
+			if (mainFile.getName().charAt(i) == '_') {
 				temp = temp + '_';
 				temp = temp + "1H-ECP_";
-			}else {
+			} else {
 				temp = temp + mainFile.getName().charAt(i);
 			}
 		}
-		File tempFile = new File(mainFile.getParent() + "/" +  temp);
+		File tempFile = new File(mainFile.getParent() + "/" + temp);
 		mainFile.renameTo(tempFile);
-		
+
 	}
-	
-	/*public void fileRename(File mainFile) {
-		String temp = "";
-		for(int i=0; i<mainFile.getName().length(); i++) {
-			if(mainFile.getName().charAt(i)=='-') {
-			}else {
-				temp = temp + mainFile.getName().charAt(i);
-			}
-		}
-		File tempFile = new File(mainFile.getParent() + temp);
-		mainFile.renameTo(tempFile);
-		
-	}*/
-	
+
+	/*
+	 * public void fileRename(File mainFile) { String temp = ""; for(int i=0;
+	 * i<mainFile.getName().length(); i++) { if(mainFile.getName().charAt(i)=='-') {
+	 * }else { temp = temp + mainFile.getName().charAt(i); } } File tempFile = new
+	 * File(mainFile.getParent() + temp); mainFile.renameTo(tempFile);
+	 * 
+	 * }
+	 */
+
 	public void fileRename2(File mainFile) {
 		File tempFile = new File(mainFile.getParent() + "/20060621_" + mainFile.getName());
 		mainFile.renameTo(tempFile);
 	}
-	
-	public static void fileUrlReadAndDownload(String fileAddress,
-			   String localFileName, String downloadDir) {
-			  OutputStream outStream = null;
-			  URLConnection uCon = null;
 
-			  InputStream is = null;
-			  try {
+	public static void fileUrlReadAndDownload(String fileAddress, String localFileName, String downloadDir) {
+		OutputStream outStream = null;
+		URLConnection uCon = null;
 
-			   System.out.println("-------Download Start------");
+		InputStream is = null;
+		try {
 
-			   URL Url;
-			   byte[] buf;
-			   int byteRead;
-			   int byteWritten = 0;
-			   Url = new URL("fileAddress");
-			   outStream = new BufferedOutputStream(new FileOutputStream(
-			     downloadDir + "\\" + localFileName));
+			System.out.println("-------Download Start------");
 
-			   uCon = Url.openConnection();
-			   is = uCon.getInputStream();
-			   buf = new byte[size];
-			   while ((byteRead = is.read(buf)) != -1) {
-			    outStream.write(buf, 0, byteRead);
-			    byteWritten += byteRead;
-			   }
+			URL Url;
+			byte[] buf;
+			int byteRead;
+			int byteWritten = 0;
+			Url = new URL("fileAddress");
+			outStream = new BufferedOutputStream(new FileOutputStream(downloadDir + "\\" + localFileName));
 
-			   System.out.println("Download Successfully.");
-			   System.out.println("File name : " + localFileName);
-			   System.out.println("of bytes  : " + byteWritten);
-			   System.out.println("-------Download End--------");
+			uCon = Url.openConnection();
+			is = uCon.getInputStream();
+			buf = new byte[size];
+			while ((byteRead = is.read(buf)) != -1) {
+				outStream.write(buf, 0, byteRead);
+				byteWritten += byteRead;
+			}
 
-			  } catch (Exception e) {
-			   e.printStackTrace();
-			  } finally {
-			   try {
-			    is.close();
-			    outStream.close();
-			   } catch (IOException e) {
-			    e.printStackTrace();
-			   }
-			  }
-			 }
+			System.out.println("Download Successfully.");
+			System.out.println("File name : " + localFileName);
+			System.out.println("of bytes  : " + byteWritten);
+			System.out.println("-------Download End--------");
 
-			 /**
-			  * 
-			  * @param fileAddress
-			  * @param downloadDir
-			  */
-			 public static void fileUrlDownload(String fileAddress, String downloadDir) {
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				is.close();
+				outStream.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 
-			  int slashIndex = fileAddress.lastIndexOf('/');
-			  int periodIndex = fileAddress.lastIndexOf('.');
+	/**
+	 * 
+	 * @param fileAddress
+	 * @param downloadDir
+	 */
+	public static void fileUrlDownload(String fileAddress, String downloadDir) {
 
-			  // 파일 어드레스에서 마지막에 있는 파일이름을 취득
-			  String fileName = fileAddress.substring(slashIndex + 1);
+		int slashIndex = fileAddress.lastIndexOf('/');
+		int periodIndex = fileAddress.lastIndexOf('.');
 
-			  if (periodIndex >= 1 && slashIndex >= 0
-			    && slashIndex < fileAddress.length() - 1) {
-			   fileUrlReadAndDownload(fileAddress, fileName, downloadDir);
-			  } else {
-			   System.err.println("path or file name NG.");
-			  }
-			 }
+		// 파일 어드레스에서 마지막에 있는 파일이름을 취득
+		String fileName = fileAddress.substring(slashIndex + 1);
 
-	
+		if (periodIndex >= 1 && slashIndex >= 0 && slashIndex < fileAddress.length() - 1) {
+			fileUrlReadAndDownload(fileAddress, fileName, downloadDir);
+		} else {
+			System.err.println("path or file name NG.");
+		}
+	}
+
 }
